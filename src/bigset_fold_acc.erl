b1c8e2fd151bf588d0b3687441da4077d3374af4
Russell Brown <russell.brown@mac.com>
2016-02-11 16:39:04 +0000

    Use the leveldb accumulator

--- src/bigset_fold_acc.erl
+++ src/bigset_fold_acc.erl
@@ -75,88 +75,9 @@ fold({Key, Value}, Acc=#fold_acc{not_found=true}) ->
     end;
 fold({Key, Val}, Acc=#fold_acc{not_found=false}) ->
     %% an element key? We've sent the clock (not_found=false)
-    #fold_acc{set=Set, key_prefix=Pref, prefix_len=PrefLen, actor=Me} = Acc,
-    case Key of
-        <<Pref:PrefLen/binary, $c, _Rest/binary>> ->
-            %% a clock for another actor, skip it
-            Acc;
-        <<Pref:PrefLen/binary, $d, Me/binary>> ->
-            %% My set tombstone, I need this!
-            Acc#fold_acc{set_tombstone=bigset:from_bin(Val)};
-        <<Pref:PrefLen/binary, $d, _NotMe/binary>> ->
-            %% Some other actors set_tombstone
-            Acc;
-        <<Pref:PrefLen/binary, $e, Rest/binary>> ->
-            {element, Set, Element, Actor, Cnt, TSB} = bigset:decode_element(Rest, Set),
-            Ctx = bigset:from_bin(Val),
-            #fold_acc{set_tombstone=SetTombstone} = Acc,
-            case bigset_clock:seen(SetTombstone, {Acc, Cnt}) of
-                false ->
-                    add(Element, Actor, Cnt, TSB, Ctx, Acc);
-                true ->
-                    %% a handing off vnode deleted this key, so it is
-                    %% as though we don't have it, just skip it, it
-                    %% will be compacted out next round
-                    Acc
-            end;
-        _ ->
-            %% The end key
-            throw({break, Acc})
-    end.
-
-%% @private For each element we most fold over all entries for that
-%% element. Entries for an element can be an add, or a remove (and if
-%% we ever get compaction working there maybe a special aggregrated
-%% tombstone too.) We merge all the Contexts of the entries (adds and
-%% removes) to get a context that represents all the "seen" additions
-%% for the element. We use the ctx to filter the dots in the Add
-%% entries. Each add entry that is not seen by the aggregaeted context
-%% "survives" and is in the local set.
-
-%% NOTE: for causal consistency with
-%% Action-At-A-Distance this all changes
-
-%% @TODO(rdb|refactor) abstract the accumulation logic for different
-%% behaviours (CC vs EC)
-add(Element, Actor, Cnt, TSB, Ctx, Acc=#fold_acc{current_elem=Element}) ->
-    %% Same element, keep accumulating info
-    #fold_acc{current_ctx=CC, current_dots=Dots} = Acc,
-    AggCtx = bigset_clock:merge(Ctx, CC),
-    AggDots = maybe_add_dots({Actor, Cnt}, Dots, TSB),
-    Acc#fold_acc{current_ctx=AggCtx, current_dots=AggDots};
-add(Element, Actor, Cnt, TSB, Ctx, Acc=#fold_acc{current_elem=_}) ->
-    %% New element, maybe store the old one
-    Acc2 = store_element(Acc),
-    Acc3 = maybe_flush(Acc2),
-    Acc3#fold_acc{current_elem=Element,
-                  current_dots=maybe_add_dots({Actor, Cnt}, [], TSB),
-                  current_ctx=Ctx}.
-
-%% Only the ?ADD dots get added to the element's list of dots
-maybe_add_dots(_Dot, Dots, ?REM) ->
-    Dots;
-maybe_add_dots(Dot, Dots, ?ADD) ->
-    [Dot | Dots].
-
-%% @private add an element to the accumulator.
-store_element(Acc=#fold_acc{current_elem=undefined}) ->
-    Acc;
-store_element(Acc) ->
-    #fold_acc{current_elem=Elem,
-              current_dots=Dots,
-              current_ctx=Ctx,
-              elements=Elements,
-              size=Size} = Acc,
-
-    %% create a regular orswot from the bigset on disk
-    Remaining = bigset_clock:subtract_seen(Ctx, Dots),
-    case Remaining of
-        [] ->
-            Acc;
-        _ ->
-            Acc#fold_acc{elements=[{Elem, Remaining} | Elements],
-                         size=Size+1}
-    end.
+    #fold_acc{elements=Elements, size=Size} = Acc,
+    maybe_flush(Acc#fold_acc{elements=[{Key, binary_to_term(Val)} | Elements],
+             size=Size+1}).
 
 %% @private if the buffer is full, flush!
 maybe_flush(Acc=#fold_acc{size=Size, buffer_size=Size}) ->
@@ -198,8 +119,7 @@ finalise(Acc=#fold_acc{not_found=true}) ->
     send(not_found, Acc),
     done(Acc);
 finalise(Acc) ->
-    AccFinal = store_element(Acc),
-    done(AccFinal).
+    done(Acc).
 
 %% @private let the caller know we're done.
 done(Acc0) ->
